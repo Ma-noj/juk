c44192f35debd3f0d5f0bab470abd35ee860f016
Manu <manojy0505@gmail.com>
2022-09-14 15:29:57 +0530

    Starter file Added

---
+++ src/main/java/com/ty/juk/JukApplication.java
@@ -0,0 +1,13 @@
+package com.ty.juk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/juk/JukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.juk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
